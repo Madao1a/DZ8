4ff5f5b04569ed5205a80baaf14a214f787db5cf
MARS <leprcoin@gmail.com>
2023-02-14 22:56:27 +0400

    task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,10 +1,8 @@
 ﻿// Найти сумму элементов от M до N, N и M заданы
-int M = 2;
-int N = 11;
-int sum=0;
-for (int i = M; i <= N; i++)
-{
-    sum=sum+i;
-}
-Console.WriteLine(sum);
 
+int Sum(int m, int n)
+{
+    if (n == m) return n;
+    return m + Sum(m + 1, n);
+}
+Console.WriteLine(Sum(1, 5));
